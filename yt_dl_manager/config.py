674374234f3569189284dcad05420aefbbb6bf21
Oliver Gassner <ogassner@outlook.at>
2025-07-26 23:09:39 +0200

    fix: move create_default_config import to the top for better readability

--- yt_dl_manager/config.py
+++ yt_dl_manager/config.py
@@ -2,6 +2,7 @@
 import configparser
 from pathlib import Path
 from platformdirs import user_config_dir
+from .create_config import create_default_config
 
 APP_NAME = "yt-dl-manager"
 CONFIG_FILE_NAME = "config.ini"
@@ -17,7 +18,6 @@ def load_config():
     config_file_path = get_config_path()
     if not config_file_path.exists():
         # If config file doesn't exist, create a default one
-        from .create_config import create_default_config  # pylint: disable=import-outside-toplevel
         create_default_config()
     config_parser.read(config_file_path)
     return config_parser
